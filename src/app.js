d4c18ac1cce3d319025121bacf399b782fa2fd29
avdelsen <121772952+avdelsen@users.noreply.github.com>
2023-02-27 10:40:42 +0100

    Api integration in searchbar

--- src/app.js
+++ src/app.js
@@ -64,6 +64,36 @@ function displayCurrentWeatherConditions(response) {
 }
 
 let apiKey = "abda35122eo2d00bt2f4d92b2a100fa2";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+let city = "Lisbon";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayCurrentWeatherConditions);
+
+function searchCity(city) {
+  let apiKey = "abda35122eo2d00bt2f4d92b2a100fa2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCurrentWeatherConditions);
+}
+
+function retrieveValues(event) {
+  event.preventDefault();
+  let city = document.querySelector("#searchbar-city").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = "abda35122eo2d00bt2f4d92b2a100fa2";
+  let currentLat = position.coords.latitude;
+  let currentLon = position.coords.longitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${currentLon}&lat=${currentLat}&key=${apiKey}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+function giveCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let searchEngine = document.querySelector("#search-engine");
+searchEngine.addEventListener("submit", retrieveValues);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", giveCurrentLocation);
