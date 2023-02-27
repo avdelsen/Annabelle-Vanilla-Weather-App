987e6a50f3008d5bc03b12ad2970c2752b5cbc61
avdelsen <121772952+avdelsen@users.noreply.github.com>
2023-02-27 10:12:28 +0100

    Added date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
+function formattedDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[now.getDay()];
+  let number = now.getDate();
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let timetime = now.getTime();
+
+  let formattedDate = `${day} ${number} ${month} ${year}, ${hour}:${minute}`;
+  return formattedDate;
+}
+let now = new Date();
+let currentDate = document.querySelector("#currentDate");
+currentDate.innerHTML = formattedDate(now);
+
 function displayCurrentWeatherConditions(response) {
   document.querySelector("#current-temperature").innerHTML = Math.round(
     response.data.temperature.current
